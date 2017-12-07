47dd948f20d8953cf3fe8c1a44e8194fc52bc6f2
Dylan Kocsis <Dylan.Kocsis@queensu.ca>
2017-12-06 21:05:51 -0800

    Final CSS changes and removal of white space and empty css blocks.

--- themes/quotesondev/footer.php
+++ themes/quotesondev/footer.php
@@ -10,15 +10,15 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<?php esc_html( 'Primary Menu' ); ?>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
-			<div class="site-info">
-				<p class='footer-info'>Brought to you by
-				<a href="<?php echo esc_url( 'https://redacademy.com/' ); ?>">
-					<?php printf( esc_html( '%s' ), 'Red Academy' ); ?></a></p>
-			</div><!-- .site-info -->
+				<div class="site-info">
+					<p class='footer-info'>Brought to you by
+						<a href="<?php echo esc_url( 'https://redacademy.com/' ); ?>">
+						<?php printf( esc_html( '%s' ), 'Red Academy' ); ?></a></p>
+				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
